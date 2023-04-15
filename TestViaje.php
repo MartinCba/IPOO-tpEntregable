9749662ee571af4d2acbf24e83dfd7a696c14a58
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2023-04-15 20:50:20 -0300

    modificaciones

--- TestViaje.php
+++ TestViaje.php
@@ -2,10 +2,8 @@
 include("Viaje.php");
 
 
-function dropdownMenu()
+function menu()
 {
-    // int $opcionElegida
-
     do {
         echo "-------------------------------------------------------------------------------------\n";
         echo "||                              [Viaje Feliz]                                      ||\n";
@@ -16,55 +14,48 @@ function dropdownMenu()
         echo "|| realizar:                                                                       ||\n";
         echo "||                                                                                 ||\n";
         echo "|| [1] Mostrar toda la información del viaje                                       ||\n";
-        echo "|| [2] Cargar un viaje nuevo                                                       ||\n";
+        echo "|| [2] Crear un viaje nuevo                                                        ||\n";
         echo "|| [3] Agregar un nuevo pasajero al viaje                                          ||\n";
-        echo "|| [4] Quitar un pasajero del viaje                                                ||\n";
-        echo "|| [5] Modificar el nombre de un pasajero                                          ||\n";
-        echo "|| [6] Modificar el apellido de un pasajero                                        ||\n";
-        echo "|| [7] Modificar el destino del viaje actual                                       ||\n";
-        echo "|| [8] Modificar la capacidad máxima de pasajeros del viaje actual                 ||\n";
-        echo "|| [9] Mostrar el código del viaje actual                                          ||\n";
-        echo "|| [10] Mostrar el destino del viaje actual                                        ||\n";
-        echo "|| [11] Mostrar la cantidad máxima permitida de pasajeros para este viaje          ||\n";
-        echo "|| [12] Mostrar todos los pasajeros que se encuentran en el viaje actual           ||\n";
+        echo "|| [4] Quitar un pasajero del viaje                      ||\n";
+        echo "|| [5] Modificar el nombre de un pasajero                ||\n";
+        echo "|| [6] Modificar el apellido de un pasajero             ||\n";
+        echo "|| [7] Modificar el código del viaje actual                                        ||\n";
+        echo "|| [8] Modificar el destino del viaje actual                                       ||\n";
+        echo "|| [9] Modificar la capacidad máxima de pasajeros del viaje actual                ||\n";
+        echo "|| [10] Mostrar el código del viaje actual                                         ||\n";
+        echo "|| [11] Mostrar el destino del viaje actual                                        ||\n";
+        echo "|| [12] Mostrar la cantidad máxima permitida de pasajeros para este viaje          ||\n";
+        echo "|| [13] Mostrar todos los pasajeros que se encuentran en el viaje actual           ||\n";
         echo "|| [0] Para finaizar el programa                                                   ||\n";
         echo "||                                                                                 ||\n";
         echo "-------------------------------------------------------------------------------------\n";
         echo "\n";
         echo "Indique la operación que desea realizar: ";
-        $opcionElegida = trim(fgets(STDIN));
+        $opcion = trim(fgets(STDIN));
 
-        // OBSERVAR: el rango varía según cantidad de opciones
-        if ($opcionElegida < 0 || $opcionElegida > 12) {
+        if ($opcion < 0 || $opcion > 13 || !ctype_digit($opcion)) {
             echo "\n";
-            // Mensaje de error cuando la opción elegida está fuera de rango
-            echo "--------------------------------------------------------------------------------\n";
-            echo "|| ¡ERROR! Usted ha ingresado un valor NO válido,                             ||\n";
-            echo "|| verifique las opciones del menú nuevamente.                                ||\n";
-            echo "--------------------------------------------------------------------------------\n";
-            stopExecution();
+            echo "-------------------------------------------------------------------------------------\n";
+            echo "|| ¡ERROR! Usted ha ingresado un valor NO válido,                                  ||\n";
+            echo "|| verifique las opciones del menú nuevamente.                                     ||\n";
+            echo "-------------------------------------------------------------------------------------\n";
+            detenerEjecucion();
         }
-        // OBSERVAR: el rango varía según cantidad de opciones
-    } while ($opcionElegida < 0 || $opcionElegida > 12);
+    } while ($opcion < 0 || $opcion > 13);
 
     echo "\n";
 
-    return $opcionElegida;
+    return $opcion;
 }
 
-function stopExecution()
+function detenerEjecucion()
 {
-    // string $presionarEnter
-
     echo "\n";
 
     do {
-        // Mensaje de parada para leer los resultados
         echo "Presione [ENTER] para continuar. ";
-        // Obligación de ingresar un valor para continuar la ejecución del código
-        $presionarEnter = trim(fgets(STDIN));
-    } while ($presionarEnter != "");
-
+        $enter = trim(fgets(STDIN));
+    } while ($enter != "");
     echo "\n";
 }
 
@@ -73,22 +64,22 @@ function stopExecution()
 /********************************************************************************/
 
 $viaje = new Viaje(513, "Madrid", 260);
-$arrayPassengers[0] = ["numero de documento" => 33456789, "nombre" => "Pepe", "apellido" => "Sanchez"];
-$arrayPassengers[1] = ["numero de documento" => 34567890, "nombre" => "Juan", "apellido" => "Gomez"];
-$arrayPassengers[2] = ["numero de documento" => 30123456, "nombre" => "Marcos", "apellido" => "Garcia"];
-$viaje->setPassengers($arrayPassengers);
+$arrayPasajeros[0] = ["numero de documento" => 33456789, "nombre" => "Pepe", "apellido" => "Sanchez"];
+$arrayPasajeros[1] = ["numero de documento" => 34567890, "nombre" => "Juan", "apellido" => "Gomez"];
+$arrayPasajeros[2] = ["numero de documento" => 30123456, "nombre" => "Marcos", "apellido" => "Garcia"];
+$viaje->setPasajeros($arrayPasajeros);
 echo "El programa cuenta con un primer viaje precargado\n";
 echo "\n";
 
 do {
-    $opcionMenu = dropdownMenu();
+    $opcionMenu = menu();
 
     switch ($opcionMenu) {
         case 1:
             // Muestra el viaje cargado actualmente
             echo $viaje;
             echo "\n";
-            stopExecution();
+            detenerEjecucion();
             break;
         case 2:
             // Crea un viaje nuevo
@@ -105,7 +96,7 @@ do {
             } else {
                 echo "ERROR: valor inválido para capacidad máxima de pasajeros\n";
             }
-            stopExecution();
+            detenerEjecucion();
             break;
         case 3:
             // Agrega un nuevo pasajero al viaje actual
@@ -117,22 +108,31 @@ do {
             $apellido = trim(fgets(STDIN));
 
             $pasajero = ["numero de documento" => $documento, "nombre" => $nombre, "apellido" => $apellido];
-            $mensaje = $viaje->addPassenger($pasajero);
-            echo $mensaje;
-            stopExecution();
+            $puedeAgregar = $viaje->agregarPasajero($pasajero);
+
+            if ($puedeAgregar) {
+                echo "El pasajero se agrego con exito\n";
+            } else {
+                if (count($viaje->getPasajeros()) == $viaje->getCantMaxPasajeros()) {
+                    echo "ERROR: el viaje se encuentra en su capacidad máxima de pasajeros\n";
+                } else {
+                    echo "ERROR: el documento del pasajero ya se encuentra registrado dentro del viaje\n";
+                }
+            }
+            detenerEjecucion();
             break;
         case 4:
             // Quita un pasajero del viaje por número de documento
             echo "Ingrese el número de documento del pasajero que desea quitar del viaje: ";
             $documento = trim(fgets(STDIN));
-            $esPosible = $viaje->removePassengerByDni($documento);
+            $esPosible = $viaje->quitarPasajeroPorDocumento($documento);
 
             if ($esPosible) {
                 echo "Pasajero quitado del viaje exitosamente\n";
             } else {
                 echo "No se pudo quitar el pasajero del viaje (no se encontro el documento cargado en el viaje actual)\n";
             }
-            stopExecution();
+            detenerEjecucion();
             break;
         case 5:
             // Modifica el nombre de un pasajero por número de documento
@@ -140,14 +140,14 @@ do {
             $documento = trim(fgets(STDIN));
             echo "Ingrese el nuevo nombre que desea asignar al pasajero: ";
             $nombre = trim(fgets(STDIN));
-            $esPosible = $viaje->modifyNameByDni($documento, $nombre);
+            $esPosible = $viaje->modificarNombrePorDocumento($documento, $nombre);
 
             if ($esPosible) {
                 echo "Se modifico el nombre del pasajero exitosamente\n";
             } else {
                 echo "No se pudo modificar el nombre del pasajero (no se encontro el documento cargado en el viaje actual)\n";
             }
-            stopExecution();
+            detenerEjecucion();
             break;
         case 6:
             // Modifica el apellido de un pasajero por número de documento
@@ -155,62 +155,70 @@ do {
             $documento = trim(fgets(STDIN));
             echo "Ingrese el nuevo apellido que desea asignar al pasajero: ";
             $apellido = trim(fgets(STDIN));
-            $esPosible = $viaje->modifyLastNameByDni($documento, $apellido);
+            $esPosible = $viaje->modificarApellidoPorDocumento($documento, $apellido);
 
             if ($esPosible) {
                 echo "Se modifico el apellido del pasajero exitosamente\n";
             } else {
                 echo "No se pudo modificar el apellido del pasajero (no se encontro el documento cargado en el viaje actual)\n";
             }
-            stopExecution();
+            detenerEjecucion();
             break;
         case 7:
+            // Modifica el código del viaje actual
+            echo "Ingrese un nuevo código para el viaje actual: ";
+            $codigo = trim(fgets(STDIN));
+            $viaje->setCodigo($codigo);
+            echo "Código de viaje modificado exitosamente";
+            detenerEjecucion();
+            break;
+        case 8:
             // Modifica el destino del viaje
             echo "Ingrese el nuevo destino del viaje: ";
             $destino = trim(fgets(STDIN));
-            $viaje->setDestination($destino);
+            $viaje->setDestino($destino);
             echo "Destino modificado exitosamente\n";
-            stopExecution();
+            detenerEjecucion();
             break;
-        case 8:
-            // Modifica la cantidad máxima perimitida de pasajeros en el viaje
+        case 9:
+            // Modifica la cantidad máxima permitida de pasajeros en el viaje
             echo "Ingrese la nueva cantidad máxima permitida de pasajeros para este viaje: ";
             $capMaxima = trim(fgets(STDIN));
-            $esPosible = $viaje->setMaxQuantity($capMaxima);
-            if ($esPosible) {
+            if (ctype_digit($capMaxima)) {
+                $viaje->setCantMaxPasajeros($capMaxima);
                 echo "Cantidad máxima de pasajeros para este viaje modificada exitosamente\n";
             } else {
                 echo "Error: valor ingresado para cantidad máxima de pasajeros invalido\n";
             }
-            stopExecution();
-            break;
-        case 9:
-            // Muestra el código del viaje actual
-            echo "El código del viaje actual es: " . $viaje->getCode() . "\n";
-            stopExecution();
+            detenerEjecucion();
             break;
         case 10:
-            // Muestra el destino del viaje actual
-            echo "El destino del viaje actual es: " . $viaje->getDestination() . "\n";
-            stopExecution();
+            // Muestra el código del viaje actual
+            echo "El código del viaje actual es: " . $viaje->getCodigo() . "\n";
+            detenerEjecucion();
             break;
         case 11:
-            // Muestra la cantidad máxima de pasajeros permitida para el viaje actual
-            echo "La cantidad máxima de pasajeros permitida para el viaje actual es: " . $viaje->getMaxQuantity() . "\n";
-            stopExecution();
+            // Muestra el destino del viaje actual
+            echo "El destino del viaje actual es: " . $viaje->getDestino() . "\n";
+            detenerEjecucion();
             break;
         case 12:
+            // Muestra la cantidad máxima de pasajeros permitida para el viaje actual
+            echo "La cantidad máxima de pasajeros permitida para el viaje actual es: " . $viaje->getCantMaxPasajeros() . "\n";
+            detenerEjecucion();
+            break;
+        case 13:
             // Muestra el arreglo de pasajeros del viaje actual
             echo "El arreglo de pasajeros del viaje actual es:\n";
-            print_r($viaje->getPassengers());
+            print_r($viaje->getPasajeros());
             echo "\n";
-            stopExecution();
+            detenerEjecucion();
             break;
         case 0:
             // Finaliza el programa
             break;
         default:
-            // En caso de error (imposible) accederá a esta opción
+            // En caso de error (imposible) accederá a esta opción y se finalizará el programa
             break;
     }
 } while ($opcionMenu != 0);
